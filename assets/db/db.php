9ee5c2621c7f53666dc743946d54ea3250484458
Edson242 <edsonsilveira619@gmail.com>
2023-06-14 19:27:19 -0300

    Atualização003

--- assets/db/db.php
+++ assets/db/db.php
@@ -8,9 +8,9 @@ define("NAME_DATABASE",  'sistema_despesa');
 // Configurações de conexão com o Banco de Dados MySQL
 $connection = mysqli_connect(HOST, USER, PASSWORD, NAME_DATABASE);
 
-if($connection){
-    echo "✅ Conectado ao servidor !";
-} else { 
-    echo "❌ Não foi possivel conectar no banco de dados !";
-}
+// if($connection){
+//     echo "✅ Conectado ao servidor !";
+// } else { 
+//     echo "❌ Não foi possivel conectar no banco de dados !";
+// }
 ?>
--- inserirDespesas.php
+++ inserirDespesas.php
@@ -16,20 +16,24 @@
       } else {
         echo "Nenhuma opção foi selecionada";
       }
-    $queryCategorias = "SELECT id FROM categorias WHERE categorias.nome = '$categoria';";
+    $queryCategorias = "SELECT categorias.id FROM categorias WHERE categorias.nome = '$categoria';";
     $processamento = mysqli_query($connection, $queryCategorias);
-    print_r($processamento);
+    // $categoriaDado = mysqli_fetch_array($processamento);
+    // print_r($processamento->fetch_assoc());
+    $categoria = $processamento->fetch_assoc();
+    $categoria = $categoria["id"];
+    // print_r($categoria);
 
 
-    $query = "INSERT INTO despesas (id_usuario, id_categoria, valor, data, descricao) VALUES (0, 1, $valor, '$data', '$descricao');";
+    $query = "INSERT INTO despesas (id_usuario, id_categoria, valor, data, descricao) VALUES (0, $categoria, $valor, '$data', '$descricao');";
 
     if (mysqli_query($connection, $query)) {
-        header("location: index.html");
+        // header("location: index.html");
         echo '<script>alert("✅ Dado inserido com sucesso !")</script>';
         // echo "✅ Dado inserido com sucesso !";
     } else {
         // echo "❌ Dado não inserido !: " . mysqli_error($connection);
-        header("location: index.html");
+        // header("location: index.html");
         echo '<script>alert("❌ Dado não inserido !")</script>';
     }
     mysqli_close($connection);
